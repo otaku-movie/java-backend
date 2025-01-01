1f403114acc8f9f678e741f85b6ac1a19388f4e9
last order <2495713984@qq.com>
2025-01-02 00:16:58 +0800

    build: 添加多环境配置和修改docker内的redis配置

--- src/main/java/com/example/backend/mapper/CinemaMapper.java
+++ src/main/java/com/example/backend/mapper/CinemaMapper.java
@@ -5,7 +5,10 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.example.backend.controller.CinemaListQuery;
 import com.example.backend.entity.Cinema;
 import com.example.backend.response.CinemaResponse;
-import com.example.backend.response.MovieShowTimeList;
+import com.example.backend.response.Spec;
+import com.example.backend.response.app.AppMovieShowTimeResponse;
+import com.example.backend.response.app.GetCinemaMovieShowTimeListResponse;
+import com.example.backend.response.cinema.MovieShowingResponse;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -16,7 +19,10 @@ public interface CinemaMapper extends BaseMapper<Cinema> {
   IPage<CinemaResponse> cinemaList(CinemaListQuery query, IPage<CinemaResponse> page);
 
   CinemaResponse cinemaDetail(Integer id);
-  List<Object> cinemaSpec(Integer cinemaId);
-
-
+  List<Spec> cinemaSpec(Integer cinemaId);
+  List<MovieShowingResponse> getMovieShowing(Integer cinemaId);
+  GetCinemaMovieShowTimeListResponse getCinemaMovieShowTimeList(
+    GetCinemaMovieShowTimeListQuery query,
+    Integer showTimeState
+  );
 }
